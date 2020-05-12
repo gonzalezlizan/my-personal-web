ac4919f7ef988a51d9b3e92475203214a99412d2
Alberto González <a.gonzalezlizan@gmail.com>
2020-05-12 08:35:12 +0200

    profile image

--- frontend/angular-app/src/app/app.component.ts
+++ frontend/angular-app/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-personal-web';
+export class AppComponent implements OnInit {
+  title = 'Alberto González';
+
+  public ngOnInit() {
+    (($) => {
+      // Smooth scrolling using jQuery easing
+      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+            location.hostname === this.hostname) {
+          let target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: (target.offset().top)
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    })(jQuery); // End of use strict
+  }
 }
