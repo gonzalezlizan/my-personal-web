9df5a3a1f3361c0e34581464a7320a23dd698ac4
Alberto González <a.gonzalezlizan@gmail.com>
2020-05-26 08:34:40 +0200

    vue font-awesome/profile page/css

--- frontend/vue-app/src/main.js
+++ frontend/vue-app/src/main.js
@@ -1,5 +1,25 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faLinkedinIn,
+  faGithub,
+  faTwitter,
+  faFacebookF
+} from '@fortawesome/free-brands-svg-icons'
+import { 
+  faCheck, 
+  faTrophy 
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faLinkedinIn, faGithub, faTwitter, faFacebookF)
+library.add(faCheck, faTrophy);
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
